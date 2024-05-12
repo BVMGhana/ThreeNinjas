b9f8807d5076c191ae5580791d603bc69cfd2f0b
Joshua Gato <joshuagato37@gmail.com>
2024-05-12 12:36:16 +0000

    Added league controller

---
+++ app/Http/Controllers/LeagueController.php
@@ -0,0 +1,117 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\League;
+use Illuminate\Http\Request;
+
+class LeagueController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+
+            $leagues = League::all();
+            return response()->json($leagues);
+
+        } catch (Exception $e) {
+            // Writing detailed error message to the developer log and 
+            // returning a simple error message to the client
+            Log::error('Error occurred: ' . $e->getMessage());
+            return response()->json(['error' => 'Oops! Something went wrong.'], 500);
+        }
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+
+            $league = new League(['title' => $request->title]);
+            $league->save();
+            return response()->json(['success' => true, 'message' => 'League added.'], 201);
+
+        } catch (Exception $e) {
+            // Writing detailed error message to the developer log and 
+            // returning a simple error message to the client
+            Log::error('Error occurred: ' . $e->getMessage());
+            return response()->json(['error' => 'Oops! Something went wrong.'], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show($id)
+    {
+        try {
+            $league =  League::findOrFail($id);
+            return response()->json($league);
+
+        } catch (\Exception $e) {
+            // Writing detailed error message to the developer log and 
+            // returning a simple error message to the client
+            Log::error('Error occurred: ' . $e->getMessage());
+            return response()->json(['error' => 'Oops! Something went wrong.'], 500);
+        }
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(League $league)
+    {
+        // 
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, $id)
+    {
+        try {
+            $league = League::findOrFail($id);
+            $league->title = $request->title;
+            $league->save();
+
+            return response()->json(['success' => true, 'message' => 'League updated.'], 201);
+
+        } catch (Exception $e) {
+            // Writing detailed error message to the developer log and 
+            // returning a simple error message to the client
+            Log::error('Error occurred: ' . $e->getMessage());
+            return response()->json(['error' => 'Oops! Something went wrong.'], 500);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy($id)
+    {
+        try {
+            $league = League::findOrFail($id);
+            $league->delete();
+            return response()->json(['success' => true, 'message' => 'League deleted.'], 200);
+
+        } catch (Exception $e) {
+            // Writing detailed error message to the developer log and 
+            // returning a simple error message to the client
+            Log::error('Error occurred: ' . $e->getMessage());
+            return response()->json(['error' => 'Oops! Something went wrong.'], 500);
+        }
+    }
+}
