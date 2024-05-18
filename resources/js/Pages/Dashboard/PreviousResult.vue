5f5270f76481b509f7d302c4379e3ecfc121e0fc
Joshua Gato <joshuagato37@gmail.com>
2024-05-18 19:17:54 +0000

    Added custom error message display

--- resources/js/Pages/Dashboard/PreviousResult.vue
+++ resources/js/Pages/Dashboard/PreviousResult.vue
@@ -4,6 +4,7 @@ import { ref } from 'vue';
 import { toast } from 'vue3-toastify';
 import 'vue3-toastify/dist/index.css';
 import axios from '../../axiosConfig';
+import InputError from '@/Components/InputError.vue';
 
 const displayMessage = (message, type) => toast(message, { autoClose: 1000, type});
 
@@ -41,19 +42,20 @@ const { url } = usePage();
 const stateUrl = ref(null);
 stateUrl.value = url;
 const editDataId = ref(null);
+const errors = ref(null);
 
 const submitPrediction = async () => {
     if (stateUrl.value.includes("previous-results")) {
         try {
             if (stateUrl.value.includes('edit=true') && editDataId) {
-                const response = await axios.put(`previous-results/${editDataId.value}`, form.data());
+                const response = await axios.put(`previous-results/${editDataId.value}`, form);
                 if (response.status === 201) {
                     displayMessage(response.data.message, 'success');
                     const result = await axios.get('previous-results');
                     previousResults.value = result.data;
                 }
             } else {
-                const response = await axios.post('previous-results', form.data());
+                const response = await axios.post('previous-results', form);
                 if (response.status === 201) {
                     displayMessage(response.data.message, 'success');
                     const result = await axios.get('previous-results');
@@ -62,7 +64,8 @@ const submitPrediction = async () => {
                 }
             }
         } catch (error) {
-            displayMessage(error.response.statusText, 'error');
+            errors.value = error.response.data.errors;
+            displayMessage(error.response.data.message, 'error');
         }
     }
 };
@@ -142,14 +145,17 @@ const addNewRecord = () => {
                         <option value="">Select League</option>
                         <option v-for="league in leagues" :key="league.id" :value="league.id">{{league.title}}</option>
                     </select>
+                    <InputError v-if="errors && errors.league_id" class="text-center" :message="errors.league_id[0]" />
 
                     <label class="heading-2" for="fixtures">Fixtures</label>
                     <input type="text" id="fixtures" v-model="form.fixtures" name="fixtures" placeholder="Fixtures">
+                    <InputError v-if="errors && errors.fixtures" class="text-center" :message="errors.fixtures[0]" />
 
                     <select id="league" name="tips" v-model="form.tip_id">
                         <option value="">Select Tip</option>
                         <option v-for="tip in tips" :key="tip.id" :value="tip.id">{{tip.title}}</option>
                     </select>
+                    <InputError v-if="errors && errors.tip_id" class="text-center" :message="errors.tip_id[0]" />
 
                     <label class="heading-2" for="result">Results</label>
                     <select id="result" name="country" v-model="form.results">
@@ -157,6 +163,7 @@ const addNewRecord = () => {
                         <option value="1">Success</option>
                         <option value="0">Failure</option>
                     </select>
+                    <InputError v-if="errors && errors.results" class="text-center" :message="errors.results[0]" />
                 
                     <div class="form-buttons">
                         <button v-if="!stateUrl.includes('edit=true')" type="reset">Clear</button>
