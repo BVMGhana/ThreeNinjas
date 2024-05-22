6cd7dcfa93a7a4795a111f1fa7eba2a295486e27
Joshua Gato <joshuagato37@gmail.com>
2024-05-22 17:00:05 +0000

    Added bottom border to analytics section and changed .env2 mail sending variables

--- resources/js/Pages/Homepage.vue
+++ resources/js/Pages/Homepage.vue
@@ -502,28 +502,28 @@ const sliderImages = ['/images/bg_1.jpg', '/images/bg_2.jpg'];
         <div class="wrapper w-full flex flex-wrap items-center justify-center gap-[10px]">
           <div class="container w-[185px] lg:w-[190px] w-[185px] lg:h-[190px]
             flex flex-col justify-around items-center py-[1em] px-0 text-[16px] rounded-[0.5em]
-              bg-[#21242b]">
+              bg-[#21242b] border-b-[10px] border-solid border-[#fc036b]">
             <i class="fas fa-users-cog text-[#fc036b] text-[2.5em]"></i>
             <span class="num text-[#fff] grid place-items-center font-semibold text-[3em]" :data-val="usersCount">0</span>
             <span class="text text-[#e0e0e0] py-[0.7em] px-0 font-normal leading-[0]">Subscribed Clients</span>
           </div>
           <div class="container w-[185px] lg:w-[190px] w-[185px] lg:h-[190px]
             flex flex-col justify-around items-center py-[1em] px-0 text-[16px] rounded-[0.5em]
-              bg-[#21242b]">
+              bg-[#21242b] border-b-[10px] border-solid border-[#fc036b]">
             <i class="fas fa-glasses text-[#fc036b] text-[2.5em]"></i>
             <span class="num text-[#fff] grid place-items-center font-semibold text-[3em]" :data-val="predictionsCount">0</span>
             <span class="text text-[#e0e0e0] py-[0.7em] px-0 font-normal leading-[0]">Predictions</span>
           </div>
           <div class="container w-[185px] lg:w-[190px] w-[185px] lg:h-[190px]
             flex flex-col justify-around items-center py-[1em] px-0 text-[16px] rounded-[0.5em]
-              bg-[#21242b]">
+              bg-[#21242b] border-b-[10px] border-solid border-[#fc036b]">
             <i class="fas fa-users text-[#fc036b] text-[2.5em]"></i>
             <span class="num text-[#fff] grid place-items-center font-semibold text-[3em]" :data-val="usersCount">0</span>
             <span class="text text-[#e0e0e0] py-[0.7em] px-0 font-normal leading-[0]">Satisfied Clients</span>
           </div>
           <div class="container w-[185px] lg:w-[190px] w-[185px] lg:h-[190px]
             flex flex-col justify-around items-center py-[1em] px-0 text-[16px] rounded-[0.5em]
-              bg-[#21242b]">
+              bg-[#21242b] border-b-[10px] border-solid border-[#fc036b]">
             <i class="fas fa-star text-[#fc036b] text-[2.5em]"></i>
             <span class="num text-[#fff] grid place-items-center font-semibold text-[3em]" :data-val="usersCount">0</span>
             <span class="text text-[#e0e0e0] py-[0.7em] px-0 font-normal leading-[0]">Five Stars</span>
