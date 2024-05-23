405720989e331d3c3db70e066bc3f94b7b7e74dd
Joshua Gato <joshuagato37@gmail.com>
2024-05-23 19:14:56 +0000

    Added components for submit button and input errors field for transparent elements

---
+++ resources/js/Components/SubmitButton.vue
@@ -0,0 +1,6 @@
+<template>
+    <button class="w-full mb-4 text-[18px] mt-6 rounded-[40px] bg-[#c9145f] text-white 
+        hover:bg-[#fc036b] py-2 transition-colors duration-300" type="submit">
+        <slot />
+    </button>
+</template>
