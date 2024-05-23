c485fd9888aaab528dd75455e189694e2aaca5e0
Joshua Gato <joshuagato37@gmail.com>
2024-05-23 19:15:38 +0000

    Updated GuestLayout

--- resources/js/Layouts/GuestLayout.vue
+++ resources/js/Layouts/GuestLayout.vue
@@ -4,7 +4,7 @@ import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
-    <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
+    <div class="min-h-screen flex flex-col justify-center items-center pt-6 sm:pt-0 bg-gray-100 dark:bg-gray-900">
         <div>
             <Link href="/">
                 <p class="text-white text-[32px] lg:text-[48px]">3 Ninjas Home</p>
@@ -12,8 +12,9 @@ import { Link } from '@inertiajs/vue3';
         </div>
 
         <div
-            class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white dark:bg-gray-800 shadow-md overflow-hidden 
-            sm:rounded-lg bg-slate-700 shadow-lg backdrop-filter backdrop-blur-sm bg-opacity-50 rounded-[12px]"
+            class="w-full sm:max-w-md mt-6 px-6 py-4 dark:bg-gray-600 shadow-md overflow-hidden 
+            sm:rounded-lg border border-slate-400 bg-slate-900 shadow-lg backdrop-filter 
+            backdrop-blur-sm bg-opacity-30 rounded-[12px]"
         >
             <slot />
         </div>
