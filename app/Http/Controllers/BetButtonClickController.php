9d8d44eabb20f409ea577be7f851ef1228df03f5
Joshua Gato <joshuagato37@gmail.com>
2024-05-27 00:00:05 +0000

    Added bet button click controller

---
+++ app/Http/Controllers/BetButtonClickController.php
@@ -0,0 +1,102 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\BetButtonClick;
+use Illuminate\Http\Request;
+
+class BetButtonClickController extends Controller
+{
+
+    public function count($company)
+    {
+        try {
+            $clicks_count = BetButtonClick::where('company', $company)->count();
+            return response()->json($clicks_count);
+            
+        } catch (Exception $e) {
+            // Writing detailed error message to the developer log and 
+            // returning a simple error message to the client
+            Log::error('Error occurred: ' . $e->getMessage());
+            return response()->json(['error' => 'Oops! Something went wrong.'], 500);
+        }
+    }
+
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'user_id' => 'required|numeric',
+            'ninja' => 'required|string|max:50',
+            'company' => 'required|string|max:100'
+        ]);
+
+        try {
+
+            $button_click = new BetButtonClick([
+                'user_id' => $request->user_id,
+                'ninja' => $request->ninja,
+                'company' => $request->company
+            ]);
+            $button_click->save();
+            return response()->json(['success' => true ], 201);
+
+        } catch (Exception $e) {
+            // Writing detailed error message to the developer log and 
+            // returning a simple error message to the client
+            Log::error('Error occurred: ' . $e->getMessage());
+            return response()->json(['error' => 'Oops! Something went wrong.'], 500);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(BetButtonClick $betButtonClick)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(BetButtonClick $betButtonClick)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, BetButtonClick $betButtonClick)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(BetButtonClick $betButtonClick)
+    {
+        //
+    }
+}
