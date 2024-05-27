e728f484041e7a2be8557408d854b13c4104b608
Joshua Gato <joshuagato37@gmail.com>
2024-05-27 01:26:03 +0000

    Added most recent users function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -7,6 +7,7 @@ use App\Models\BlackNinja;
 use App\Models\RedNinja;
 use App\Models\WhiteNinja;
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class UserController extends Controller
 {
@@ -34,9 +35,32 @@ class UserController extends Controller
     public function count()
     {
         try {
+            $now = Carbon::now();
+            $yesterday = $now->subDays(1);
+
+            $countLast24Hours = User::where('created_at', '>=', $yesterday)->count();
+
+            $countBefore24Hours = User::where('created_at', '<', $yesterday)->count();
+
+            $percentageLast24Hours = 0;
+            if ($countBefore24Hours > 0) {
+                $percentageLast24Hours = ($countLast24Hours / $countBefore24Hours) * 100;
+            } else {
+                if ($countLast24Hours == 0) {
+                    $percentageLast24Hours = 0;
+                } else {
+                    $percentageLast24Hours = 100;
+                }
+            }
 
             $users_count = User::count();
-            return response()->json($users_count);
+
+            return response()->json([
+                'users_count' => $users_count,
+                'countLast24Hours' => $countLast24Hours,
+                'countBefore24Hours' => $countBefore24Hours,
+                'percentageLast24Hours' => $percentageLast24Hours
+            ]);
 
         } catch (\Exception $e) {
             // Writing detailed error message to the developer log and 
@@ -70,11 +94,29 @@ class UserController extends Controller
     /**
      * Return the three most recently registered users.
      */
+    public function most_recent()
+    {
+        try {
+
+            $most_recent_users = User::latest()->take(3)->get();
+            return response()->json($most_recent_users);
+
+        } catch (\Exception $e) {
+            // Writing detailed error message to the developer log and 
+            // returning a simple error message to the client
+            Log::error('Error occurred: ' . $e->getMessage());
+            return response()->json(['error' => 'Oops! Something went wrong.'], 500);
+        }
+    }
+
+    /**
+     * Return the ten recently registered users.
+     */
     public function recent()
     {
         try {
 
-            $recent_users = User::latest()->take(3)->get();
+            $recent_users = User::latest()->take(10)->get();
             return response()->json($recent_users);
 
         } catch (\Exception $e) {
