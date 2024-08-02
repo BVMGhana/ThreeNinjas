cb75ba3f268905f7e8ebb219779ffdf873e6af06
Joshua Gato <joshuagato37@gmail.com>
2024-08-02 16:56:20 +0000

    Added google tag to index page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -168,5 +168,15 @@
                 </div>
             </div>
         </div>
+
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-JFW4BKKNPC"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-JFW4BKKNPC');
+        </script>
     </body>
 </html>
