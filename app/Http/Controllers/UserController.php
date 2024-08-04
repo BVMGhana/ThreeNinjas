de6f87e34aad3e68d15753686e9093f311f6a52a
Joshua Gato <joshuagato37@gmail.com>
2024-08-04 01:45:41 +0000

    Updated user controller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,8 +6,11 @@ use App\Models\User;
 use App\Models\BlackNinja;
 use App\Models\RedNinja;
 use App\Models\WhiteNinja;
+use App\Models\Prediction;
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Http;
+use App\Http\Controllers\PredictionController;
 
 class UserController extends Controller
 {
@@ -76,17 +79,43 @@ class UserController extends Controller
     public function count_all()
     {
         try {
-            $white_ninja_count = WhiteNinja::count();
-            $red_ninja_count = RedNinja::count();
-            $black_ninja_count = BlackNinja::count();
+            $white_ninja_count = Prediction::where("ninja", "white")->first()->count;
+            $red_ninja_count = Prediction::where("ninja", "red")->first()->count;
+            $black_ninja_count = Prediction::where("ninja", "black")->first()->count;
             $total_predictions = $white_ninja_count + $red_ninja_count + $black_ninja_count;
             return response()->json($total_predictions);
+
+            // $app_url = env('SERVER_BASE_URL', '');
+
+            // $predictions = Http::get("{$app_url}/api/predictions-count");
+            // $predictions = Http::get("{$app_url}/api/users-count");
+            // return response()->json($this->count());
+            // $predictions = Http::get("http://127.0.0.1:8000/api/users-count");
+            // $predictions = Http::get("https://3ninjaodds.com/api/users-count");
+
+            // $predictions = Http::withOptions(['verify' => false])->get("https://3ninjaodds.com/api/users-count");
+            // $predictions = Http::withOptions(['verify' => false])->get("http://localhost:8000/api/users-count");
+            // $predictions = Http::withOptions(['verify' => false])->get("http://127.0.0.1:8000/api/users-count-aa");
+            
+            // return response()->json($predictions->json());
+
+            // This works
+            // $request = new Request(["ninja" => "red", "count" => 0]);
+            // $predictionController = new PredictionController();
+            // $predictionController->store($request);
+            // return response()->json($request);
+
+            // This works
+            // $request = new Request();
+            // $predictionController = new PredictionController();
+            // $predictionController->update($request, "white");
+            // return response()->json($predictionController);
 
         } catch (\Exception $e) {
             // Writing detailed error message to the developer log and 
             // returning a simple error message to the client
             Log::error('Error occurred: ' . $e->getMessage());
-            return response()->json(['error' => 'Oops! Something went wrong.'], 500);
+            return response()->json(['error' => $e->getMessage()], 500);
         }
     }
 
